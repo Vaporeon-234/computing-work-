ebe814c22410666b1be14019ea337dfed799e1cc
Vaporeon-234 <69793646+Vaporeon-234@users.noreply.github.com>
2020-10-22 13:43:41 +0800

    Update YN's chatbot-app.py

--- YN's chatbot-app.py
+++ YN's chatbot-app.py
@@ -12,18 +12,32 @@ while counter == 0:
     if each_word == "sad" and counter == 0:
       print("Back in my day, if an 'ol seadog was sad, we'll drink to the gods!")
       print("Tomorrow will be a better day, we would say.")
+      print("You wanna' have some?")
+      print("...")
+      print("No?")
+      print("Well, your loss. But remember, there's always a fresh supply here!")
+      print("*wink*")
       counter = 1
     if each_word == "happy" and counter == 0:
       print("Well," + name + ", look's like it's time for some rum!")
-      print("Yes, the good stuff......")
+      print("Yes, the good stuff.....")
+      print("Huh? Oh yah, you're not like us.")
+      print("Uh..... here's a cookie to chew on then.")
+      print("Uh..... take another one.....")
       counter = 1
     if each_word == "tired" and counter == 0:
       print("Working your butt off the whole day? Hardworking, I'se say.")
       print("You should remember to hole up once in a while, lad")
+      print("Otherwise..... * lowers voice *")
+      print("Your' bones will melt and all the meat falls out in clumps....")
+      print("...")
+      print("Now don't look so sad, lad. It's just a story.")
+      print("But it just might happen to you.....")
       counter = 1
     if counter == 0:
       print("What? I don't know what you are saying!")
       print("Oh nevermind. Your feelings can stay to yourself.")
+      print("You can tell me them later.")
       counter = 1      
 while counter == 1:
   description = input("You look troubled, is that nasty shark biting your leg again?")
